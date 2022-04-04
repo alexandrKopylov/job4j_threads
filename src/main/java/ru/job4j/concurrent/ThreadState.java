0431576e88cbfa910a128f20c099fcfe44101fd2
Sasha <sash-2007@ya.ru>
2022-04-04 11:56:16 +0500

    1.1. Состояние нити. [#229175 #283367]-3

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -12,10 +12,11 @@ public class ThreadState {
         first.start();
         second.start();
 
-        while (first.getState() == Thread.State.TERMINATED
-                && second.getState() == Thread.State.TERMINATED) {
-            System.out.println("Work finish");
+        while (first.getState() != Thread.State.TERMINATED
+                || second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getState());
+            System.out.println(second.getState());
         }
-
+        System.out.println("Work finish");
     }
 }
