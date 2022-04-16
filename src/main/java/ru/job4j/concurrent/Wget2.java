42bc33f9b7d488f5138bdd2a6a74358f8cfc47d8
alexandrKopylov <sash-2007@ya.ru>
2022-04-16 07:24:44 +0500

    4. Скачивание файла с ограничением. [#144271 #286635]-2

--- src/main/java/ru/job4j/concurrent/Wget2.java
+++ src/main/java/ru/job4j/concurrent/Wget2.java
@@ -4,6 +4,8 @@ import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Wget2 implements Runnable {
     private final String url;
@@ -16,12 +18,14 @@ public class Wget2 implements Runnable {
 
     @Override
     public void run() {
+        Path path = Paths.get(url);
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("web.txt")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(String.valueOf(path.getFileName()))) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
 
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+
                 long start = System.currentTimeMillis();
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
                 long diff = System.currentTimeMillis() - start;
@@ -36,6 +40,10 @@ public class Wget2 implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("wrong count arguments (there should be 2 arguments:" +
+                    "URL and Speed downloading)");
+        }
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
         Thread wget = new Thread(new Wget2(url, speed));
